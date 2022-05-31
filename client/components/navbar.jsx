9597dace4635d67ba30671b3a225892262904470
Kyle Parrent <kppp433@gmail.com>
2022-05-31 00:54:10 +0000

    Part.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -13,8 +13,8 @@ export default class Navbar extends React.Component {
       <nav className="navbar navbar-expand-md bg-dark navbar-dark">
         <div className='container'>
           <a href="#" className="navbar-brand">THE OPINION</a>
-          <a href='#'></a>
-          <div className="collapse navbar-collapse">
+          <button className="navbar-toggler" type='button' data-bs-toggle="collapse" data-bs-target="#navmenu"><span className="navbar-toggler-icon"></span></button>
+          <div className="collapse navbar-collapse" id="navmenu">
             <ul className="navbar-nav ms-auto">
               <li className="nav-item">
                 <a href="#" className="nav-link">Top Headings</a>
