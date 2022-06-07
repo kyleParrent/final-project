52915fb4620f879038d43a7b8de7f5138cfea74a
Kyle Parrent <kppp433@gmail.com>
2022-06-07 00:57:37 +0000

    Revised.

--- client/components/review.jsx
+++ client/components/review.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 
 export default class Review extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      theButtonClass: '',
-      theButtonText: ''
-    };
-  }
-
   componentDidMount() {
     if (this.props.review.rating === 'inform') {
       this.setState({ theButtonClass: 'inform no-button me-4 mt-2 text-dark', theButtonText: 'Informative' });
@@ -18,13 +10,22 @@ export default class Review extends React.Component {
   }
 
   render() {
+    let theButtonClass;
+    let theButtonText;
+    if (this.props.review.rating === 'inform') {
+      theButtonClass = 'inform no-button me-4 mt-2 text-dark';
+      theButtonText = 'Informative';
+    } else {
+      theButtonClass = 'persuade no-button me-4 mt-2';
+      theButtonText = 'Persuasive';
+    }
     return (
       <div className='row m-3'>
         <div className="col-12 bg-secondary rounded-3 text-white">
           <div className="row">
             <div className="col-12 d-flex align-items-center">
-              <div className={this.state.theButtonClass}>
-                {this.state.theButtonText}
+              <div className={theButtonClass}>
+                {theButtonText}
               </div>
               <p className='mb-0 mt-2'>By: Fake Username Joe</p>
             </div>
